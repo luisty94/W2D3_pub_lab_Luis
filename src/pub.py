6aac8dd78af1dccdc9cc96a84a732fb74fcee634
james yates <yates_7@hotmail.com>
2022-10-27 00:33:33 +0100

    first extension done

--- src/pub.py
+++ src/pub.py
@@ -8,9 +8,19 @@ class Pub:
     def check_till(self):
         return self.till
 
-    def increase_till(self, amount):
-        self.till += amount
+    def increase_till(self, drinks):
+        for drink in drinks:
+            self.till += drink.price
 
     def check_age(self, customer_age):
         # self.age = customer_age
         return customer_age >= 18
+
+    def serve_drinks(self, customer, drinks):
+        if customer.drunkenness < 15:
+                return customer.buy_drinks(drinks)
+        else:
+            return "refuse service"
+
+
+    
